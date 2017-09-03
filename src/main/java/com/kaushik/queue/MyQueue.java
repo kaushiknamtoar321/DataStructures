11a1a54d7641454eac216cbd8c2c333a6d055037
Kaushik <kaushiknamtoar321@gmail.com>
2017-09-03 11:52:55 +0530

    comment
    comment

--- src/main/java/com/kaushik/queue/MyQueue.java
+++ src/main/java/com/kaushik/queue/MyQueue.java
@@ -1,7 +1,7 @@
 package com.kaushik.queue;
 
 public class MyQueue { 
-	public static void main(String[] args) {
+	public static void main(String[] args) {//new method  
 		System.out.println("hi"); 
 	}
 }
