107f13ac1cb39d5ae3258fc4e3d769167a1c6c99
Kaushik <kaushiknamtoar321@gmail.com>
2017-09-05 01:12:25 +0530

    Singly Linked list
    Added class

--- src/main/java/com/kaushik/linkedlist/singlylinkedlist/SinglyLinkedList.java
+++ src/main/java/com/kaushik/linkedlist/singlylinkedlist/SinglyLinkedList.java
@@ -4,58 +4,80 @@ public class SinglyLinkedList<T> {
 
 	private Node<T> head;
 	private int size;
-	private int capacity = 7;
+	private int capacity = 100;
 
-	private void extendCapacity() {
-		if ((this.capacity - this.size) < 5) {
-			this.capacity = this.capacity + 20;
+	private void expandList() {
+		if (capacity - size < 5) {
+			capacity = capacity + 20;
 		}
 	}
 
 	public void add(T data) {
-		extendCapacity();
-		Node<T> temp = this.head;
+		expandList();
+		Node<T> temp = head;
 		while (true) {
 			if (temp == null) {
-				this.head = new Node<>();
-				this.head.setElement(data);
-				this.size++;
+				head = new Node<>();
+				head.setElement(data);
+				size++;
 				break;
-			} else if (temp.getNext() == null) {
-				Node<T> node = new Node<>();
-				node.setElement(data);
-				temp.setNext(node);
-				this.size++;
+			}else if(temp.getNext() == null) {
+				Node<T> n = new Node<>();
+				n.setElement(data); 
+				temp.setNext(n);
+				size++;
 				break;
 			} else {
 				temp = temp.getNext();
 			}
 		}
 	}
-
+	
 	public void traverse() {
-		Node<T> temp = this.head;
-		while (true) {
-			if (temp != null) {
-				System.out.println(temp.element.toString());
-				temp = temp.getNext();
-			} else {
-				break;
+		if(size == 0) {
+			System.out.println("List is empty");
+		} else {
+			Node<T> temp = head;
+			while(true) {
+				if(temp == null) {
+					break;
+				} else {
+					System.out.println(temp.getElement());
+					temp = temp.getNext();
+				}
 			}
 		}
 	}
-
-	public void get(int index) throws Exception {
-		Node<T> temp = this.head;
-		if (temp == null) {
-			throw new Exception("List is empty");
-		} else if (index > this.size) {
+	
+	public void get(int index) {
+		if(index > size) {
 			throw new ArrayIndexOutOfBoundsException();
 		} else {
+			Node<T> temp = head;
 			int count = 0;
 			while (true) {
-				if (count == index) {
-					System.out.println(temp.getElement().toString());
+				if(count == index) {
+					System.out.println(temp.getElement());
+					break;
+				}else {
+					temp = temp.getNext();
+					count++;
+				}
+			}
+		}
+	}
+	
+	public void remove(int index) {
+		if(index > size) {
+			throw new ArrayIndexOutOfBoundsException();
+		} else {
+			Node<T> temp = head;
+			int count = 0;
+			while(true) {
+				if(count == index-1) {
+					Node<T> n = temp.getNext();
+					temp.setNext(n.getNext());
+					System.out.println("Removing : "+ n.getElement());
 					break;
 				} else {
 					temp = temp.getNext();
@@ -65,43 +87,21 @@ public class SinglyLinkedList<T> {
 		}
 	}
 	
-	public void delete(int index) throws Exception{
-		Node<T> temp = this.head;
-		if (temp == null) {
-			throw new Exception("List is empty");
-		} else if (index > this.size) {
-			throw new ArrayIndexOutOfBoundsException();
-		} else {
-			int count = 0;
-			while (true) {
-				if (count == index-1) {
-					Node<T> e = temp.getNext(); 
-					temp.setNext(e.getNext());
-					
-					System.out.println("Removing : "+e.getElement().toString());
-					break;
-				} else {
-					temp = temp.getNext();
-					count++;
-				}
-			}
-		}
-	}
-
-	public static void main(String[] args) throws Exception {
+	
+	public static void main(String[] args) {
 		SinglyLinkedList<Integer> linkedList = new SinglyLinkedList<>();
-		linkedList.add(11);
-		linkedList.add(55);
-		linkedList.add(5);
-		linkedList.add(6);
-		linkedList.traverse();
-		System.out.println();
-		linkedList.get(0);
-		System.out.println();
-		linkedList.delete(1);
+		linkedList.add(22);
+		linkedList.add(4);
+		linkedList.add(54);
+		linkedList.add(7);
+		linkedList.add(31);
 		System.out.println();
 		linkedList.traverse();
-
+		System.out.println();
+		linkedList.get(2);
+		linkedList.remove(3);
+		System.out.println();
+		linkedList.traverse();
 	}
 
 	class Node<T> {
